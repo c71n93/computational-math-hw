7fbadd50cefec3143e717ce233c248500b986110
Roman <korostinskii.r@phystech.edu>
2022-11-25 20:00:44 +0300

    finish lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,8 +1,11 @@
 import numpy as np
+from numpy import linalg as la
 from sympy import *
 
 
 ############### nonlinear equation ###############
+
+
 x = symbols('x')
 
 iterative_formula = 1 / (2 ** x)
@@ -13,6 +16,7 @@ IterativeFuntionDiff = lambdify(x, iterative_formula_diff)
 
 ksi = 0.5 
 Q = IterativeFuntionDiff(ksi) # to evaluate the condition of achieving accuracy
+
 
 def AccuracyCondition(x_n_plus1, x_n, q = Q):
     return abs(x_n_plus1 - x_n) / (1 - q)
@@ -29,14 +33,47 @@ def SimpleIterativeMethod(x_0, epsilon = 1e-16):
     return res, iters
 
 
+############### system of nonlinear equations ###############
+
+
+def F(x):
+    F = np.array([[0.0], [0.0]])
+    F[0] = np.sin(x[0] + 1.0) - x[1] - 1.2
+    F[1] = 2 * x[0] + np.cos(x[1]) - 2.0
+    return F
+
+def J(x):
+    J = np.identity(2)
+    J[0, 0] = np.cos(x[0] + 1.0)
+    J[0, 1] = -1.0
+    J[1, 0] = 2.0
+    J[1, 1] = -np.sin(x[1])
+    return J
+
+def Norm3(x):
+    return la.norm(x, 2)
+
+def NewtonMethod(startPoint, F, J, epsilon, iterations):
+    res = startPoint
+
+    for i in range(0, iterations):
+        temp   = res
+        res = res - np.matmul(np.linalg.inv(J(res)), F(res))
+        if(Norm3(res - temp) < epsilon):
+            return res, i
+
+    return res
+
+
 ############### main ###############
 
 
 def main():
-    x_0 = 1
-    epsilon = 1e-14
-    res, iters = SimpleIterativeMethod(x_0, epsilon)
-    print("x =", res, ";", "number of iteraitions:", iters, ";", "epsilon:", epsilon)
+    res, iters = SimpleIterativeMethod(1, 1e-14)
+    print("nonlinear equation:", "x =", res, ";", "number of iteraitions:", iters, ";", "epsilon:", epsilon)
+    res, iters = NewtonMethod(np.array([[0], [0]]), F, J, 1e-3, 500)
+    print("system of nonlinear equations:", "x =", res, ";", "number of iteraitions:", iters, ";", "epsilon:", 1e-3)
+    return
 
 if __name__ == '__main__':
     main()
