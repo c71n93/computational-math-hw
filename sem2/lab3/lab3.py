98072b3e8024e69cb90393af4b434c9ca7fa3d81
c71n93 <korostinskii.r@phystech.edu>
2023-04-27 00:27:46 +0300

    sem2:lab3 finished lab3

--- sem2/lab3/lab3.py
+++ sem2/lab3/lab3.py
@@ -5,9 +5,10 @@ import time
 
 def System(x, vec):
   f = np.array([0.0, 0.0])
-  f[0] = vec[0]
+  f[0] = vec[1]
   f[1] = -0.5 * vec[1]**2 / (1 - 0.5 * vec[0])
   return f
+
 
 def RungeKuttaIteration_4Order(x_n, vec_n, f, h = 0.01):
   f_1 = f(x_n, vec_n)
@@ -18,7 +19,8 @@ def RungeKuttaIteration_4Order(x_n, vec_n, f, h = 0.01):
   vec_n_1 = vec_n + h / 6 * (f_1 + 2 * f_2 + 2 * f_3 + f_4)
   return vec_n_1
 
-def SolutionsRK(vec_start, x_start = 0.0, x_end = 1.0, h = 0.01):
+
+def SolutionsRK(vec_start, x_start = 0.0, x_end = 1.0, h = 0.001):
   x_num = int(x_end / h)
 
   x_array = np.array([x_start])
@@ -31,40 +33,31 @@ def SolutionsRK(vec_start, x_start = 0.0, x_end = 1.0, h = 0.01):
 
   return vec_array, x_array
 
-def BinSolutionSearch(z0_left, z0_right, y0, y1 = 0, abs_tol = 0.001):
+
+def BinSolutionSearch(z0_left, z0_right, y0, y1 = 0, abs_tol = 1e-8):
   z0_middle = (z0_left + z0_right) / 2
   new_sol, new_x_array = SolutionsRK(vec_start = np.array([y0, z0_middle]))
   y1_new = new_sol[-1][0] # y(1)
-  print("y'(0) = " + str(z0_middle) + " y(1) = " + str(y1_new))
   if abs(y1_new - y1) < abs_tol:
-    return new_sol, new_x_array, z0_middle
-  elif y1_new > y1:
+    return new_sol, new_x_array
+  elif y1_new < y1:
     return BinSolutionSearch(z0_middle, z0_right, y0)
   else:
     return BinSolutionSearch(z0_left, z0_middle, y0)
-  
-def IterationSolutionSearch():
-  y0 = 0.25
-  arr = np.arange(-1, 0.05, 0.05)
-  for i in arr:
-    new_sol, new_x_array = SolutionsRK(vec_start = np.array([y0, i]))
-    y_array = new_sol.transpose()[0]
-    ShowSoulutionPlots(y_array, new_x_array, new_sol[0][1])
-    print("")
-    
-def ShowSoulutionPlots(y, x, p):
-  # clear_output(wait=True)
+
+
+def ShowSoulutionPlots(y, x, p, y1):
+  clear_output(wait=True)
   plt.figure(figsize=[12, 5], dpi=100)
   plt.plot(x, y, label = "y")
   plt.xlabel("x")
   plt.ylabel("y")
   plt.legend()
-  plt.title("Метод стрельбы. y(x), y'(0) = " + str(p))
+  plt.title(f"y(x): y'(0) = {p}, y(1) = {y1}")
   plt.grid()
   plt.show()
 
-# y0 = 0.25
-# sol_array, x_array, p = BinSolutionSearch(-1.0, 0.0, y0 = y0)
-# y_array = sol_array.transpose()[0]
-# ShowSoulutionPlots(y_array, x_array, p)
-IterationSolutionSearch()
+y0 = 0.25
+sol_array, x_array, p = BinSolutionSearch(-1.0, 0.0, y0 = y0)
+y_array = sol_array.transpose()[0]
+ShowSoulutionPlots(y_array, x_array, p)
